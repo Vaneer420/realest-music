22cf0b0497a7c343a04458429b2cd37b625387f1
Vaneer <97142095+Vaneer420@users.noreply.github.com>
2024-03-16 15:13:22 -0400

    Removal Command Patches
    im gonna have nightmares about this nesting
    
    anyways, heres the list of bugs patched
    
    - NaN values being passed as the index no longer remove the currently playing song
    - I was under the assumption that "o" would be undefined if it didn't return any elements. It returned an empty array instead, which has now been fixed.
    - Fixed negative numbers (and 1 because that's the currently playing song) being passed as the index
    
    this is horrible to look at but it works so im pushing for now

--- commands/remove.js
+++ commands/remove.js
@@ -7,23 +7,34 @@ module.exports = {
 	execute(message, args, client, queue) {
 		const embed = api.prepareEmbedMessage(client);
 
-		if(!queue.songs.length > 0) {
+		if(Number(args[0]) == NaN) {
 			embed.setTitle('Command Error')
-				.setDescription('The queue is currently empty.');
-
-			return message.channel.send({embeds: [embed]});
+				.setDescription('Please enter a valid index.');
 		} else {
-			const o = queue.songs.splice(Number(args[0]) - 1, 1);
+			if(!queue.songs.length > 0) {
+				embed.setTitle('Command Error')
+					.setDescription('The queue is currently empty.');
 
-			if(o != undefined) {
-				embed.setTitle('Song Removed')
-					.setDescription(`The song has been successfully removed from the queue.`);
 			} else {
-				embed.setTitle('Command Failed')
-					.setDescription('No deleted items were returned. Did you provide the right index?');
-			}
+				let index_to_remove = Number(args[0]) - 1;
+				if(index_to_remove < 1) {
+					embed.setTitle('Command Error')
+						.setDescription('You cannot remove indexes less than 2 from the queue!');
+				} else {
+					const o = queue.songs.splice(index_to_remove, 1);
 
-			return message.channel.send({embeds: [embed]});
+					if(o.length != 0) {
+						embed.setTitle('Song Removed')
+							.setDescription(`The song has been successfully removed from the queue.`);
+
+					} else {
+						embed.setTitle('Command Failed')
+							.setDescription('No deleted items were returned. Did you provide the right index?');
+					}
+				}
+
+				return message.channel.send({embeds: [embed]});
+			}
 		}
 	}
 }
