1215bdd87d64ca84dfca7c34568ab557e536d988
WilliamAnimate <75321465+WilliamAnimate@users.noreply.github.com>
2024-03-16 15:52:56 -0400

    fix: crashes. style: indentation & use 1 tab instead.

--- commands/remove.js
+++ commands/remove.js
@@ -1,37 +1,38 @@
 const api = require("../api.js");
 
 module.exports = {
-  name: 'remove',
-  description: 'Removes a specified index from the queue, 0 being the current song playing and onwards.',
-  usage: 'm!remove <#>',
-  execute(message, args, client, queue) {
-    const embed = api.prepareEmbedMessage(client);
+	name: 'remove',
+	description: 'Removes a specified index from the queue, 0 being the current song playing and onwards.',
+	usage: 'm!remove <#>',
+	execute(message, args, client, queue) {
+		const embed = api.prepareEmbedMessage(client);
+		let isItPermissibleToRemoveItemsFromThisSpecificMusicQueueInTheCurrentExecutionContextBooleanVariable = true;
 
-    if (isNaN(Number(args[0])) || !queue.songs.length) {
-      embed.setTitle('Command Error');
-      if (isNaN(Number(args[0]))) {
-        embed.setDescription('Please enter a valid index.');
-      } else {
-        embed.setDescription('The queue is currently empty.');
-      }
-      return message.channel.send({ embeds: [embed] });
-    }
+		if(isNaN(Number(args[0])) || !queue.songs.length) {
+			embed.setTitle('Command Error');
+			if(isNaN(Number(args[0]))) {
+				embed.setDescription('Please enter a valid index.');
+			} else {
+				embed.setDescription('The queue is currently empty.');
+			}
+			return message.channel.send({ embeds: [embed] });
+		}
 
-    let indexToRemove = Number(args[0]) - 1;
-    if (indexToRemove < 0) {
-      indexToRemove = 0;
-    }
+		let indexToRemove = Number(args[0]) - 1;
+		if(indexToRemove < 1) {
+			embed.setTitle('Command Error')
+				.setDescription('You cannot remove indexes less than 2 from the queue!');
+			isItPermissibleToRemoveItemsFromThisSpecificMusicQueueInTheCurrentExecutionContextBooleanVariable = false;
+		}
 
-    const removedSong = queue.songs.splice(indexToRemove, 1);
-    if (removedSong.length) {
-      embed.setTitle('Song Removed')
-          .setDescription(`The song "${removedSong[0].title}" has been successfully removed from the queue.`);
-    } else {
-      embed.setTitle('Command Failed')
-          .setDescription('No song was removed. The provided index might be invalid.');
-    }
+		if(isItPermissibleToRemoveItemsFromThisSpecificMusicQueueInTheCurrentExecutionContextBooleanVariable) {
+			const removedSong = queue.songs.splice(indexToRemove, 1);
 
-    return message.channel.send({ embeds: [embed] });
-  }
+			if(removedSong.length) embed.setTitle('Song Removed').setDescription(`The song "${removedSong[0].title}" has been successfully removed from the queue.`);
+			else embed.setTitle('Command Failed').setDescription('No song was removed. The provided index might be invalid.');
+		}
+
+		return message.channel.send({ embeds: [embed] });
+	}
 };
 
