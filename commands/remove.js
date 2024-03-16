e382494fa5bf5289609f96b5c583168a93af23c9
WilliamAnimate <75321465+WilliamAnimate@users.noreply.github.com>
2024-03-16 15:24:10 -0400

    refactor: remove the indentation hell commited in 22cf0b0

--- commands/remove.js
+++ commands/remove.js
@@ -1,40 +1,37 @@
 const api = require("../api.js");
 
 module.exports = {
-	name: 'remove',
-	description: 'Removes a specified index from the queue, 0 being the current song playing and onwards.',
-	usage: 'm!remove <#>',
-	execute(message, args, client, queue) {
-		const embed = api.prepareEmbedMessage(client);
+  name: 'remove',
+  description: 'Removes a specified index from the queue, 0 being the current song playing and onwards.',
+  usage: 'm!remove <#>',
+  execute(message, args, client, queue) {
+    const embed = api.prepareEmbedMessage(client);
 
-		if(Number(args[0]) == NaN) {
-			embed.setTitle('Command Error')
-				.setDescription('Please enter a valid index.');
-		} else {
-			if(!queue.songs.length > 0) {
-				embed.setTitle('Command Error')
-					.setDescription('The queue is currently empty.');
+    if (isNaN(Number(args[0])) || !queue.songs.length) {
+      embed.setTitle('Command Error');
+      if (isNaN(Number(args[0]))) {
+        embed.setDescription('Please enter a valid index.');
+      } else {
+        embed.setDescription('The queue is currently empty.');
+      }
+      return message.channel.send({ embeds: [embed] });
+    }
 
-			} else {
-				let index_to_remove = Number(args[0]) - 1;
-				if(index_to_remove < 1) {
-					embed.setTitle('Command Error')
-						.setDescription('You cannot remove indexes less than 2 from the queue!');
-				} else {
-					const o = queue.songs.splice(index_to_remove, 1);
+    let indexToRemove = Number(args[0]) - 1;
+    if (indexToRemove < 0) {
+      indexToRemove = 0;
+    }
 
-					if(o.length != 0) {
-						embed.setTitle('Song Removed')
-							.setDescription(`The song has been successfully removed from the queue.`);
+    const removedSong = queue.songs.splice(indexToRemove, 1);
+    if (removedSong.length) {
+      embed.setTitle('Song Removed')
+          .setDescription(`The song "${removedSong[0].title}" has been successfully removed from the queue.`);
+    } else {
+      embed.setTitle('Command Failed')
+          .setDescription('No song was removed. The provided index might be invalid.');
+    }
 
-					} else {
-						embed.setTitle('Command Failed')
-							.setDescription('No deleted items were returned. Did you provide the right index?');
-					}
-				}
+    return message.channel.send({ embeds: [embed] });
+  }
+};
 
-				return message.channel.send({embeds: [embed]});
-			}
-		}
-	}
-}
