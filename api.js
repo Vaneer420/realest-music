d2858b1a1df11dda92bd8993664cfbbf4c0762d4
Vaneer <97142095+Vaneer420@users.noreply.github.com>
2024-06-14 14:31:59 -0400

    indentation fixes
    spaces keep getting into here aaaaaaa

--- api.js
+++ api.js
@@ -73,24 +73,24 @@ module.exports = {
 	 * @returns object containing information on whether user should be rate limited and how many seconds are between their last command and command before that
 	 */
 	checkDebounce(authorId) {
-        let returnValue = {
-            shouldRateLimit: false,
-            diff: 0,
-        };
+		let returnValue = {
+			shouldRateLimit: false,
+			diff: 0,
+		};
 
-        const currentTime = Math.floor(Date.now() / 1000);
-        const timeBefore = debounceGaming[authorId];
-        let count = debounceGaming[`${authorId}_count`] || 0;
-        const diff = currentTime - timeBefore;
-        if(diff < process.env.debounce_minimal_timeout) {
-            if(count > process.env.debounce_activation_threshold) {
-                returnValue.diff = diff;
-                returnValue.shouldRateLimit = true;
-                return returnValue;
-            }
-        }
-        debounceGaming[authorId] = currentTime;
-        debounceGaming[`${authorId}_count`] = count + 1;
-        return returnValue;
-    }
+		const currentTime = Math.floor(Date.now() / 1000);
+		const timeBefore = debounceGaming[authorId];
+		let count = debounceGaming[`${authorId}_count`] || 0;
+		const diff = currentTime - timeBefore;
+		if(diff < process.env.debounce_minimal_timeout) {
+			if(count > process.env.debounce_activation_threshold) {
+				returnValue.diff = diff;
+				returnValue.shouldRateLimit = true;
+				return returnValue;
+			}
+		}
+		debounceGaming[authorId] = currentTime;
+		debounceGaming[`${authorId}_count`] = count + 1;
+		return returnValue;
+	}
 };
--- commands/loop.js
+++ commands/loop.js
@@ -1,13 +1,13 @@
 const api = require('../api.js');
 
 const LoopStatus = {
-    Enabled: "set_enabled_true",
-    Disabled: "set_enabled_false",
+	Enabled: "set_enabled_true",
+	Disabled: "set_enabled_false",
 }
 
 const LoopMode = {
-    Queue: "set_mode_queue",
-    Song: "set_mode_song",
+	Queue: "set_mode_queue",
+	Song: "set_mode_song",
 }
 
 module.exports = {
@@ -15,30 +15,30 @@ module.exports = {
 	description: 'Allows you to toggle looping a specific song.',
 	usage: 'm!loop <off, queue, song>',
 	alias: 'l',
-    category: 'PLAYBACK CONTROLS',
+	category: 'PLAYBACK CONTROLS',
 	execute(message, args, client) {
 		var looping = api.loopControl();
 		var mode = args[0];
 		const embed = api.prepareEmbedMessage(client);
 
-        switch(mode) {
-            case 'queue':
-                api.loopControl(LoopStatus.Enabled);
-                api.loopControl(LoopMode.Queue);
-                embed.setTitle('Looping Enabled').setDescription('Looping has been enabled for the queue.');
-                break;
-            case 'song':
-                api.loopControl(LoopStatus.Enabled);
-                api.loopControl(LoopMode.Song);
-                embed.setTitle('Looping Enabled').setDescription('Looping has been enabled for this song.');
-                break;
-            case 'off':
-                api.loopControl(LoopStatus.Disabled);
-                embed.setTitle('Looping Disabled').setDescription('Looping has been disabled.');
-                break;
-            default:
-                return api.errorEmbed('Please provide a valid option!', embed, message);
-        }
+		switch(mode) {
+			case 'queue':
+				api.loopControl(LoopStatus.Enabled);
+				api.loopControl(LoopMode.Queue);
+				embed.setTitle('Looping Enabled').setDescription('Looping has been enabled for the queue.');
+				break;
+			case 'song':
+				api.loopControl(LoopStatus.Enabled);
+				api.loopControl(LoopMode.Song);
+				embed.setTitle('Looping Enabled').setDescription('Looping has been enabled for this song.');
+				break;
+			case 'off':
+				api.loopControl(LoopStatus.Disabled);
+				embed.setTitle('Looping Disabled').setDescription('Looping has been disabled.');
+				break;
+			default:
+				return api.errorEmbed('Please provide a valid option!', embed, message);
+		}
 
 		return message.channel.send({embeds: [embed]});
 	}
